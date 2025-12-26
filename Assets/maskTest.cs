13c16a90d81d32650d7a84bb958f6c14ddb8827f
Vincent-207 <vhannah2007@outlook.com>
2025-12-26 12:51:29 -0600

    magnifying glass

--- Assets/maskTest.cs
+++ Assets/maskTest.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class maskTest : MonoBehaviour
 {
+    [SerializeField]
+    GameObject childImage;
+    Vector3 childImagePos;
+    RectTransform childRectTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        childRectTransform = childImage.GetComponent<RectTransform>();
+        childImagePos = childRectTransform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        childRectTransform.position = childImagePos;
     }
 }

2e51fbfc411f0087c852fe965cb04228e0eaff6b
Vincent-207 <vhannah2007@outlook.com>
2025-12-26 12:47:44 -0600

    Toggle/scaling for serial number

--- Assets/ScaleTween.cs
+++ Assets/ScaleTween.cs
@@ -1,16 +1,42 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ScaleTween : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    bool isZoomedIn = false;
+    [SerializeField]
+    float tweenDuration;
+    [SerializeField]
+    AnimationCurve growCurve, lessenCurve;
+    float currentTweenTime;
+    public void toggleZoom()
     {
-        
+        // if in tween, reset time correctly. 
+        if(currentTweenTime < tweenDuration)
+        {
+            currentTweenTime = 1 - currentTweenTime;
+        }
+        else
+        {
+        currentTweenTime = 0;
+            
+        }
+        isZoomedIn = !isZoomedIn;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        currentTweenTime += Time.deltaTime;
+        float normalizedTimePos = math.min(currentTweenTime / tweenDuration, 1f);
+        if(isZoomedIn)
+        {
+            transform.localScale = Vector3.one * growCurve.Evaluate(normalizedTimePos);
+        }
+        else
+        {
+            transform.localScale = Vector3.one * lessenCurve.Evaluate(normalizedTimePos);
+            
+        }
         
     }
 }
